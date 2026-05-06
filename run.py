29f542b82c939e4d8dbf5d086d760ee33596415a
ShivaGanesh.V <vsgvarikuppala@gmail.com>
2026-05-06 21:45:53 +0530

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-# run.py
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    # Run without the reloader; the development reloader can pick up changes
-    # in the virtualenv or system packages and cause unexpected restarts.
-    app.run(debug=True, use_reloader=False, port=5003)
